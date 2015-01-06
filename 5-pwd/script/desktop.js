43a3792cf3cf9c58be36997c4d2696b8ed875835
pe22dw <pe22dw@student.lnu.se>
2015-01-06 16:39:42 +0000

    styling and experimenting with opening a window

--- 5-pwd/script/desktop.js
+++ 5-pwd/script/desktop.js
@@ -3,12 +3,42 @@
 var Desktop = {
     
     init: function() {
-        
+      
+      Desktop.openWindow();  
     },
     
     createDesktop: function() {
         
         
+    },
+    
+    openWindow: function() {
+      
+      var myWindow = document.createElement("div");
+      myWindow.setAttribute("id", "window");
+      
+      var myWindowTop = document.createElement("div");
+      myWindowTop.setAttribute("id", "windowtop");
+      
+      var headerText = document.createElement("p");
+      headerText.innerHTML = "Hej Nisse";
+      myWindowTop.appendChild(headerText);
+      
+      
+      
+      
+      
+      var myWindowBottom = document.createElement("div");
+      myWindowBottom.setAttribute("id", "windowbottom");
+      
+      myWindow.appendChild(myWindowTop);
+      myWindow.appendChild(myWindowBottom);
+      
+      
+      var myDesktop = document.getElementById("desktop");
+      
+      myDesktop.appendChild(myWindow);
+      
     },
     
 };
