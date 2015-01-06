8650d49aa4fba6aebb79e4270e5dd9dfb6541e94
pe22dw <pe22dw@student.lnu.se>
2015-01-06 15:09:05 +0000

    started with project

---
+++ 5-pwd/script/desktop.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var Desktop = {
+    
+    init: function() {
+        
+    },
+    
+    createDesktop: function() {
+        
+        
+    },
+    
+};
+
+window.onload = Desktop.init;
