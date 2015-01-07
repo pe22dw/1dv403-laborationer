cd9e9a2d9b74d28a4c5b47e9ee6b7e441d14f608
pe22dw <pe22dw@student.lnu.se>
2015-01-07 10:36:20 +0000

    solved how to close the window in two different ways

--- 5-pwd/script/desktop.js
+++ 5-pwd/script/desktop.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var Desktop = {
+  
+    clickCounter: 0,
     
     init: function() {
       
@@ -28,36 +30,83 @@ var Desktop = {
     
     openWindow: function(myDesktop) {
       
-      var myWindow = document.createElement("div");
-      myWindow.setAttribute("id", "window");
+      Desktop.clickCounter += 1;
       
-      var myWindowTop = document.createElement("div");
-      myWindowTop.setAttribute("id", "windowtop");
+      if(Desktop.clickCounter === 1)
+      {
       
-      var headerLogo = document.createElement("img");
-      headerLogo.setAttribute("src", "pics/cameraicon.png");
-      headerLogo.setAttribute("id", "headerlogo");
-      
-      var headerText = document.createElement("p");
-      headerText.innerHTML = "Image Viewer";
-      
-      var exitLogo = document.createElement("img");
-      exitLogo.setAttribute("src", "pics/closewindow.png");
-      exitLogo.setAttribute("id", "exitlogo");
-      
-      var myWindowBottom = document.createElement("div");
-      myWindowBottom.setAttribute("id", "windowbottom");
-      
-      myWindowTop.appendChild(headerLogo);
-      myWindowTop.appendChild(headerText);
-      myWindowTop.appendChild(exitLogo);
-      
-      myWindow.appendChild(myWindowTop);
-      myWindow.appendChild(myWindowBottom);
-      
-      myDesktop.appendChild(myWindow);
+        var myWindow = document.createElement("div");
+        myWindow.setAttribute("id", "window");
+        
+        var myWindowTop = document.createElement("div");
+        myWindowTop.setAttribute("id", "windowtop");
+        
+        var headerLogo = document.createElement("img");
+        headerLogo.setAttribute("src", "pics/cameraicon.png");
+        headerLogo.setAttribute("id", "headerlogo");
+        
+        var headerText = document.createElement("p");
+        headerText.innerHTML = "Image Viewer";
+        
+        var exitLogo = document.createElement("img");
+        exitLogo.setAttribute("src", "pics/closewindow.png");
+        exitLogo.setAttribute("id", "exitlogo");
+        
+        var myWindowBottom = document.createElement("div");
+        myWindowBottom.setAttribute("id", "windowbottom");
+        
+        myWindowTop.appendChild(headerLogo);
+        myWindowTop.appendChild(headerText);
+        myWindowTop.appendChild(exitLogo);
+        
+        myWindow.appendChild(myWindowTop);
+        myWindow.appendChild(myWindowBottom);
+        
+        myDesktop.appendChild(myWindow);
+        
+        exitLogo.addEventListener("click", function() {
+        
+          Desktop.closeWindow();
+        
+        }, false);
+      }
+      else
+      {
+        Desktop.closeWindow();
+      }
     },
-    
+
+    closeWindow: function() {
+      
+      document.getElementById("buttonclicked").setAttribute("id", "button");
+      document.getElementById("window").setAttribute("id", "closewindow");
+      Desktop.clickCounter = 0;
+    },
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.onload = Desktop.init;
